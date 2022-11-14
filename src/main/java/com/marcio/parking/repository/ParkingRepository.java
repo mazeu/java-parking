942602d7a932d9ef3d2411ce0681b8a507d30e3f
mazeu <marciomazeu@hotmail.com>
2022-11-14 17:43:10 -0300

    alterações na classe service e implantação da interface repository

---
+++ src/main/java/com/marcio/parking/repository/ParkingRepository.java
@@ -0,0 +1,11 @@
+package com.marcio.parking.repository;
+
+import com.marcio.parking.model.Parking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParkingRepository extends JpaRepository<Parking, String> {
+
+
+}
--- src/main/java/com/marcio/parking/service/ParkingService.java
+++ src/main/java/com/marcio/parking/service/ParkingService.java
@@ -2,22 +2,26 @@ package com.marcio.parking.service;
 
 import com.marcio.parking.exception.ParkingNotFoundException;
 import com.marcio.parking.model.Parking;
+import com.marcio.parking.repository.ParkingRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
 public class ParkingService {
 
-    private static Map<String, Parking> parkingMap = new HashMap();
+    private final ParkingRepository parkingRepository;
 
-      public List<Parking> findAll() {
-        return parkingMap.values().stream().collect(Collectors.toList());
+    public ParkingService(ParkingRepository parkingRepository) {
+        this.parkingRepository = parkingRepository;
+    }
+
+
+    public List<Parking> findAll() {
+        return parkingRepository.findAll();
     }
 
     private static String getUUID() {
@@ -25,39 +29,36 @@ public class ParkingService {
     }
 
     public Parking findById(String id) {
-        Parking parking = parkingMap.get(id);
-        if(parking == null){
-            throw new ParkingNotFoundException(id);
-        }
-        return parkingMap.get(id);
-
-
+        return parkingRepository.findById(id).orElseThrow(()->
+                new ParkingNotFoundException(id));
     }
 
     public Parking create(Parking parkingCreate) {
         String uuid = getUUID();
         parkingCreate.setId(uuid);
         parkingCreate.setEntryDate(LocalDateTime.now());
-        parkingMap.put(uuid, parkingCreate);
+        parkingRepository.save(parkingCreate);
         return parkingCreate;
     }
 
     public void deleteById(String id) {
         findById(id);
-        parkingMap.remove(id);
+        parkingRepository.deleteById(id);
     }
 
     public Parking update(String id, Parking parkingCreate) {
         Parking parking = findById(id);
         parking.setColor(parkingCreate.getColor());
-        parkingMap.replace(id, parking);
+        parking.setState(parkingCreate.getState());
+        parking.setModel(parkingCreate.getModel());
+        parking.setLicense(parkingCreate.getLicense());
+        parkingRepository.save(parking);
         return parking;
     }
 
     public Parking exit(String id) {
-        //recuperar o estacionamento pelo id
-        //atualizar data de saida
-        //calcular o valor
+
+
         return null;
     }
 }
